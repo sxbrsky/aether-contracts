e4454190c083e746d6bc8ed697792d0e736ff701
Dominik <dominikszamburski99@gmail.com>
2024-11-19 06:38:01 +0000

    Use get-packages script instead get-modified-packages (#10)

---
+++ DI/Exception/ContainerException.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the aether/aether.
+ *
+ * Copyright (C) 2024 Dominik Szamburski
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Aether\Contracts\DI\Exception;
+
+use Psr\Container\ContainerExceptionInterface;
+
+class ContainerException extends RuntimeException implements ContainerExceptionInterface
+{
+}
---
+++ DI/Factory.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the aether/aether.
+ *
+ * Copyright (C) 2024 Dominik Szamburski
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+namespace Aether\Contracts\DI;
+
+interface Factory
+{
+    /**
+     * Initializes a new instance of requested class using binding and set of parameters.
+     *
+     * @param class-string<T>|string $abstract
+     *  The unique identifier for the entry.
+     * @param array<string, array|object|scalar|null> $parameters
+     *  Parameters to construct a new class.
+     *
+     * @return ($abstract is class-string ? T : int|float|string|callable|object|null)
+     *
+     * @throws \Aether\DI\Definition\Exception\CircularDependencyException
+     * @throws \Aether\Contracts\DI\Exception\EntryNotFoundException
+     *
+     * @template T of object
+     */
+    public function make(string $abstract, array $parameters = []): int|float|string|callable|object|null;
+}
